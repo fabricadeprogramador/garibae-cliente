02016257ddb72abd32fa5957a0b422ae1e037cfd
Wilson Baez <wab.estacio@gmail.com>
2019-11-02 10:02:57 -0400

    primeiro commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <h1>GARIBAÊ TELA INICIAL</h1>
+<div>
+
+</div>  promoçẽs <div>
+
+</div> mais proximos </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
