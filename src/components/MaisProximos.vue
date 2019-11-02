11ef1a627705e603b171be0c84510c7b5d1e3b19
Matheuslm10 <matheus_lm10@hotmail.com>
2019-11-02 12:08:08 -0400

    Criação do componente card do lava-jato proximo

---
+++ src/components/MaisProximos.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="card-lava-jato">
+        <v-row>
+            <v-avatar>
+                <img
+                    src="http://www.guiafeurosa.com.br/files/guia/21/lava-jato-do-zue-em-feu-rosa-serra-es.jpg"
+                    alt="Logo do Lava-jato"
+                >
+            </v-avatar>
+            <div>
+                <p alt="Nome do lava-jato">Nome do Lava-jato</p>
+                <p alt="Status do lava-jato">Aberto</p>
+                <p alt="Distância">2,2 km</p>
+                <p alt="Preço do serviço mais barato">R$ 25,00</p>
+            </div>
+        </v-row>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MaisProximos",
+        data: () => ({})
+    };
+</script>
+
+<style>
+    .card-lava-jato {
+         background-color: white;
+         padding: 10px 20px;
+         margin: 15px;
+         border-radius: 10px;
+     }
+
+    .card-lava-jato p {
+        margin-bottom: 0px !important;
+        font-size: 13px;
+    }
+    .v-avatar {
+        margin: 0 15px !important;
+    }
+
+    .v-avatar img{
+        heigth: 70px;
+        width: 70px;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="home">
- <h1>GARIBAÊ TELA INICIAL (PRÓXIMOS)</h1>
-<div>
+    <h1>GARIBAÊ TELA INICIAL</h1>
 
-</div>  promoçẽs <div>
+    <div>  promoçẽs </div>
+    <mais-proximos></mais-proximos>
 
-</div> mais proximos </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 
+import MaisProximos from "../components/MaisProximos";
 export default {
   name: 'home',
   components: {
-    
+      MaisProximos
   }
 }
 </script>
+
+<style>
+    .v-content {
+        background-color: grey;
+    }
+</style>
